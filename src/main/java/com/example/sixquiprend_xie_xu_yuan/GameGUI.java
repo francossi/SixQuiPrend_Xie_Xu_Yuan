44f0df56deb7b0d958f0461a30bf8c24321d5063
zejunxie <xiezejun980@gmail.com>
2023-06-01 12:00:16 +0200

    computerTurn

--- src/main/java/com/example/sixquiprend_xie_xu_yuan/GameGUI.java
+++ src/main/java/com/example/sixquiprend_xie_xu_yuan/GameGUI.java
@@ -1,5 +1,7 @@
 package com.example.sixquiprend_xie_xu_yuan;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -7,10 +9,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-
 
 public class GameGUI extends Application {
     private List<List<Card>> middleCards; // 存储中间四行卡牌的数据结构
@@ -25,21 +27,14 @@ public class GameGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         BorderPane root = new BorderPane();
-
-
         currentPlayerBullheadsLabel = new Label();
         computerPlayerBullheadsLabel = new Label();
-
         root.setLeft(currentPlayerBullheadsLabel);
         root.setRight(computerPlayerBullheadsLabel);
-
-
-
         List<Card> initialMiddleCards = new ArrayList<>(); // 存储中间的四行卡牌
 
         // 发放初始卡牌给玩家和中间的四行卡牌
         dealInitialCards(players, initialMiddleCards);
-//        System.out.println(initialMiddleCards);
         middleCards = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
             List<Card> row = new ArrayList<>();
@@ -50,7 +45,6 @@ public class GameGUI extends Application {
         currentPlayerHandContainer = new HBox();
         root.setBottom(currentPlayerHandContainer);
         for (Card card : currentPlayer.getHand()) {
-            // ... Add card image to the currentPlayerHandContainer
             Image cardImage = new Image(getClass().getResource("/com/example/sixquiprend_xie_xu_yuan/card/" + card.getNumber() + ".png").toExternalForm());
             ImageView imageView = new ImageView(cardImage);
             imageView.setOnMouseClicked(event -> {
@@ -64,12 +58,7 @@ public class GameGUI extends Application {
 
         Player computerPlayer = players.get(1); // Assuming the computer player is at index 1
         for (Card card : computerPlayer.getHand()) {
-            // ... Add card image to the computerPlayerHandContainer
-            // ... Add card image to the currentPlayerHandContainer
             ImageView backCardImageView = new ImageView(new Image(getClass().getResource("/com/example/sixquiprend_xie_xu_yuan/card/backside.png").toExternalForm()));
-//            backCardImageView.setOnMouseClicked(event -> {
-//                handleCardClick(card); // 处理点击事件
-//            });
             computerPlayerHandContainer.getChildren().add(backCardImageView);
         }
 
@@ -93,13 +82,11 @@ public class GameGUI extends Application {
         // 将中间四行卡牌的容器添加到游戏界面的适当位置
         Scene scene = new Scene(root, 800, 800);
         primaryStage.setScene(scene);
-
         primaryStage.show();
     }
 
 
     private void dealInitialCards(List<Player> players, List<Card> middleCards) {
-
         Deck deck = new Deck();
         deck.shuffle();
 
@@ -108,7 +95,6 @@ public class GameGUI extends Application {
                 Card card = deck.deal();
                 player.receiveCard(card);
             }
-
         }
 
         for (int i = 0; i < 4; i++) {
@@ -130,7 +116,6 @@ public class GameGUI extends Application {
             Player computerPlayer = new Player("电脑" + i); // 假设使用Player类，并传入玩家名称
             players.add(computerPlayer);
         }
-
         return players;
     }
     // 处理卡牌点击事件
@@ -158,16 +143,6 @@ public class GameGUI extends Application {
         }
 
 
-        // 检查是否游戏结束
-        if (currentPlayer.getBullheads() > 66) {
-            System.out.println("Game Over, " + currentPlayer.getName() + " lost!");
-            System.exit(0);  // 退出程序
-        }
-
-
-
-        handleComputerPlayerTurn();
-
             // 如果添加后的卡牌数超过5张，那么玩家需要拿走这行的卡牌
         if (closestRowCards != null && closestRowCards.size() > 5) {  // 注意这里检查了 closestRowCards 是否为 null
             // 保存第六张卡
@@ -177,59 +152,55 @@ public class GameGUI extends Application {
             for (int i = 0; i < 5; i++) {
                 Card c = closestRowCards.get(i);
                 currentPlayer.setBullheads(currentPlayer.getBullheads() + c.getBullheads());
-                currentPlayerBullheadsLabel.setText(currentPlayer.getName() + "的牛头数: " + currentPlayer.getBullheads());
+
             }
             closestRowCards.clear();  // 清空这行的卡牌
 
             // 把第六张卡作为新的第一张卡
             closestRowCards.add(sixthCard);
+            Timeline timeline2 = new Timeline(new KeyFrame(Duration.seconds(1), event1 -> {
+                updateMiddleCardRows();
+                currentPlayerBullheadsLabel.setText(currentPlayer.getName() + "的牛头数: " + currentPlayer.getBullheads());
+            }));
+            timeline2.play();
+
         }
 
+        // 检查是否游戏结束
+        if (currentPlayer.getBullheads() > 66) {
+            System.out.println("Game Over, " + currentPlayer.getName() + " lost!");
+            System.exit(0);  // 退出程序
+        }
 
+        handleComputerPlayerTurn();
     }
 
     // 处理电脑玩家回合
     private void handleComputerPlayerTurn() {
         Player computerPlayer = players.get(1); // 获取电脑玩家对象
 
-        // 创建一个显示背面的 ImageView 对象
-        ImageView backCardImageView = new ImageView(new Image(getClass().getResource("/com/example/sixquiprend_xie_xu_yuan/card/backside.png").toExternalForm()));
-
-        // 将显示背面的牌添加到电脑玩家手牌容器中
-        computerPlayerHandContainer.getChildren().add(backCardImageView);
-
         // 在电脑玩家手牌中随机选择一张卡牌
         Card selectedCard = computerPlayer.getHand().get((int) (Math.random() * computerPlayer.getHand().size()));
-
-        // 从电脑玩家手牌中移除选中的卡牌
-        computerPlayer.getHand().remove(selectedCard);
-        System.out.println("The computer have played a card : "+selectedCard.getNumber());
-
-        // 更新电脑玩家手牌的显示
-        updatePlayerHand(computerPlayer);
-
-        // 创建一个显示正面的 ImageView 对象
-        ImageView frontCardImageView = new ImageView(new Image(getClass().getResource("/com/example/sixquiprend_xie_xu_yuan/card/" + selectedCard.getNumber() + ".png").toExternalForm()));
 
         // 将显示正面的牌添加到中间四行的相应位置
         int closestRow = findClosestRow(selectedCard.getNumber());
         List<Card> closestRowCards = null; // 在这里声明 closestRowCards 变量
-
+        Timeline timeline1 = new Timeline(new KeyFrame(Duration.seconds(1), event1 -> {
+            updateMiddleCardRows();
+            updatePlayerHand(computerPlayer);
+        }));
         if (closestRow != -1) {
             closestRowCards = middleCards.get(closestRow); // 在这里赋值
-            HBox rowContainer = (HBox) middleCardRows.getChildren().get(closestRow);
-            rowContainer.getChildren().add(frontCardImageView);
+            closestRowCards.add(selectedCard);
+            timeline1.play();
+
+            // 从电脑玩家手牌中移除选中的卡牌
+            computerPlayer.getHand().remove(selectedCard);
+            System.out.println("The computer have played a card : "+selectedCard.getNumber()+"\n");
+
         } else {
             System.out.println("No suitable row found for the card");
         }
-
-        // 移除显示背面的牌
-        computerPlayerHandContainer.getChildren().remove(backCardImageView);
-
-        // 计算牛头数
-        //computerPlayer.setBullheads(computerPlayer.getBullheads() + selectedCard.getBullheads());
-        //computerPlayerBullheadsLabel.setText(computerPlayer.getName() + "的牛头数: " + computerPlayer.getBullheads());
-
 
         // 如果添加后的卡牌数超过5张，那么电脑需要拿走这行的卡牌
         if (closestRowCards != null && closestRowCards.size() > 5) {  // 注意这里检查了 closestRowCards 是否为 null
@@ -240,17 +211,16 @@ public class GameGUI extends Application {
             for (int i = 0; i < 5; i++) {
                 Card c = closestRowCards.get(i);
                 computerPlayer.setBullheads(computerPlayer.getBullheads() + c.getBullheads());
-                computerPlayerBullheadsLabel.setText(computerPlayer.getName() + "的牛头数: " + computerPlayer.getBullheads());
             }
             closestRowCards.clear();  // 清空这行的卡牌
 
             // 把第六张卡作为新的第一张卡
             closestRowCards.add(sixthCard);
-        }
-
-        // 将卡牌添加到选择的行
-        if (closestRowCards != null) {  // 注意这里检查了 closestRowCards 是否为 null
-            closestRowCards.add(selectedCard);
+            Timeline timeline2 = new Timeline(new KeyFrame(Duration.seconds(5), event1 -> {
+                updateMiddleCardRows();
+                computerPlayerBullheadsLabel.setText(computerPlayer.getName() + "的牛头数: " + computerPlayer.getBullheads());
+            }));
+            timeline2.play();
         }
 
         // 检查是否游戏结束
@@ -266,8 +236,6 @@ public class GameGUI extends Application {
         if (player == currentPlayer) {
             playerHandContainer = currentPlayerHandContainer;
         } else {
-            // 根据玩家对象获取相应的手牌容器
-            // ...
             playerHandContainer=computerPlayerHandContainer;
         }
 
@@ -290,25 +258,17 @@ public class GameGUI extends Application {
                 playerHandContainer.getChildren().add(imageView);
             }
         }
-
     }
 
     // 查找与给定卡牌值最接近的行
     private int findClosestRow(int cardValue) {
         int closestRow = -1;
         int closestDiff = Integer.MAX_VALUE;
-//        System.out.println(middleCards.size());
-//        System.out.println(closestDiff);
-//        System.out.println(middleCards);
-
         for (int i = 0; i < middleCards.size(); i++) {
             List<Card> rowCards = middleCards.get(i);
-//            System.out.println(rowCards);
             if (!rowCards.isEmpty()) {
                 int lastCardValue = rowCards.get(rowCards.size() - 1).getNumber();
                 int diff = Math.abs(cardValue - lastCardValue);
-//                System.out.println(diff);
-//                System.out.println(lastCardValue);
                 if (diff < closestDiff) {
                     closestDiff = diff;
                     closestRow = i;
