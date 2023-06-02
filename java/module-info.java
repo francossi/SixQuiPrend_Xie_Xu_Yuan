44721c256799d2e20e757d247ef0474539fa6a5d
zejunxie <xiezejun980@gmail.com>
2023-06-02 11:16:08 +0200

    final version

--- java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sixquiprend_xie_xu_yuan {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sixquiprend_xie_xu_yuan to javafx.fxml;
-    exports com.example.sixquiprend_xie_xu_yuan;
-}
